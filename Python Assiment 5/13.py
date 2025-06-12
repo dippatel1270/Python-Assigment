237adc62d3447c70a724d24af572e4d9341e0aa4
dippatel1270 <deeppatel1270@gmail.com>
2025-06-12 09:32:43 +0530

    Update 13.py

--- Python Assiment 5/13.py
+++ Python Assiment 5/13.py
@@ -1,1 +1,11 @@
 # Write a generator function that generates the first 10 even numbers.
+def evennum() :
+    count = 0 
+    number = 0 
+    while count<10 :
+        yield number 
+        count+=1
+        number+=2
+
+for even_number in evennum() :
+    print(even_number,end=" ")
