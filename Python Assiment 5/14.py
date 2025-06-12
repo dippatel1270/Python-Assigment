aeb4669de27746f8702c8507bd8a45704875b4c2
dippatel1270 <deeppatel1270@gmail.com>
2025-06-12 09:33:17 +0530

    Update 14.py

--- Python Assiment 5/14.py
+++ Python Assiment 5/14.py
@@ -1,1 +1,29 @@
 # Write a Python program that uses a custom iterator to iterate over a list of integers.
+lst_num = [1,2,3,4,5,6,7,8,9]
+
+def generator(lst_num) :
+    for i in lst_num :
+        yield i
+
+val = generator(lst_num)
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
+print(next(val))
