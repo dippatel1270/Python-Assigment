a673ac79c056b6057ed0792a0996518e36866f0a
dippatel1270 <deeppatel1270@gmail.com>
2025-06-20 10:29:05 +0530

    Add files via upload

---
+++ Python Assiment 6/9.py
@@ -0,0 +1,11 @@
+# Write a Python program to import the math module and use functions like sqrt(), ceil(),floor().
+import math 
+print("Square root",math.sqrt(24))
+print("ceil",math.ceil(12))
+print("Floor",math.floor(-10))
+
+
+# Write a Python program to generate random numbers using the random module
+import random
+number = random.randint(1,100)
+print(number)
